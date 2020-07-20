52095659f90ae2b25e767b7441da264470c21945
robbbowes <up666724@myport.ac.uk>
2020-07-20 18:14:41 +0200

    Add ProductDetailComponent and routing

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -3,7 +3,6 @@ import { IProduct } from './product';
 import { ProductService } from './product.service';
 
 @Component({
-  selector: 'pm-products',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
@@ -45,7 +44,6 @@ export class ProductListComponent implements OnInit {
       },
       error: err => this.errorMessage = err
     });
-
   }
 
   toggleImage(): void {
